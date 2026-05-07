4de346103ea0c44145d9b59d4cab69a70210095b
줴령 <gpfud09@gmail.com>
2026-05-07 13:49:57 +0900

    Java 배열 - 배열 기초 - 2차원 배열

---
+++ src/array/ArrayBasic.java
@@ -0,0 +1,74 @@
+package array;
+
+import java.util.Arrays;
+
+public class ArrayBasic {
+
+    public static void main(String[] args) {
+
+        // 1. 배열 변수 선언
+        int[] numbers;
+
+        // 2. 배열 객체 생성
+        numbers = new int[5];
+
+        // 3. 배열 내부 초기화
+        // 인덱스를 지목해서 데이터를 할당.
+        // 인덱스는 0부터 시작해서 1씩 증가
+        numbers[0] = 50;
+        numbers[1] = 77;
+        numbers[2] = (int)3.14; // double을 int로 변환해서 값을 할당. 소숫점 탈락
+        numbers[3] = numbers[0] + 20;
+//        numbers[4] = "메롱"; (x)
+        numbers[4] = 100;
+//        numbers[5] = 10; (x) -> 빨간줄 안뜨니까 조심 !
+
+        // 4. 배열의 길이
+        System.out.println("배열의 길이 : " + numbers.length);
+
+        // 5. 배열의 순회
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.println((i + 1) + "번째의 값: " + numbers[i]);
+        }
+
+        System.out.println("====================================");
+
+        // 배열의 순회 forEach (향상된 for)
+        // 무조건 범위는 배열의 처음부터 끝까지로 설정(중간에 break, continue 사용 가능)
+        for(int n : numbers) {
+            System.out.println(n);
+        }
+
+        // 배열 내부를 문자열 형태로 한 번에 출력하기
+        System.out.println(Arrays.toString(numbers));
+
+        // 배열 쉽게 초기화하기
+        // 선언 당시에 크기가 고정되고, 그 크기는 변할 수 없다.
+        // 만약 혹시라도 크기를 늘리거나 줄이고 싶다면 배열을 새로 만들어서 값을 옮겨야 한다.
+        int[] pointList = {10, 20, 30, 40};
+        String[] pets = {"멍멍이", "야옹이", "짹짹이", "춘식이"};
+//        pets = {"멍멍이", "야옹이", "짹짹이"}; (x)
+        pets = new String[]{"멍멍이", "야옹이", "짹짹이"}; // (o)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
