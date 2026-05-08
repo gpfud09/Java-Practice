fa90f9754a1139ef961a0d47d280581ae2ce84b6
줴령 <gpfud09@gmail.com>
2026-05-08 10:13:31 +0900

    Java 객체지향 (정보 은닉과 캡슐화) - 정보 은닉과 캡슐화를 위한 setter 메서드 작성 - 값을 돌려주기 위한 getter 메서드 작성 - title 유효성 검증을 위한 private 메서드 작성 - 3항 연산식

--- src/oop/access_modifier/LearningLog.java
+++ src/oop/access_modifier/LearningLog.java
@@ -17,28 +17,66 @@ public class LearningLog {
     }
 
     LearningLog(String title, int minutes, boolean publicLog) {
-        this.title = title;
+        this.title = normalizeTitle(title);
         this.minutes = minutes;
         this.publicLog = publicLog;
     }
 
-    // private 접근 제한을 지정하니까 제대로 된 값도 수정이 불가능한 것을 확인함.
+    // 2. private 접근 제한을 지정하니까 제대로 된 값도 수정이 불가능한 것을 확인함.
     // 필드 값을 대신 받아서 할당하고, 값을 돌려줄 수 있는 메서드를 활용해서 값을 보호함.
     // 이때 사용하는 메서드의 이름을 getter, setter라고 한다.
+    // set + 필드이름으로 짓는 것이 관례.
 
-    public void setMinutes(int minutes) {
-        if(minutes <= 0) {
+    public void extendStudy(int additionalMinutes) {
+        if(additionalMinutes <= 0) {
             System.out.println("잘못된 공부 시간입니다.");
             return; // void 메서드에서 return은 메서드를 강제 종료한다.
         }
-        this.minutes += minutes;
+        this.minutes += additionalMinutes;
+    }
+    
+    public void changeTitle(String newTitle) {
+        this.title = normalizeTitle(newTitle);
+    }
+
+    // 이 메서드는 굳이 외부에서 알 필요가 없고, 호출할 일도 없기 때문에 private으로 설정해서
+    // 이 클래서 안에서만 사용할 수 있도록 범위를 지정.
+    private static String normalizeTitle(String newTitle) {
+        if(newTitle == null || newTitle.isBlank()) {
+            return "제목 없음";
+        }
+        return newTitle;
+    }
+
+    public void openToPublic() {
+        this.publicLog = true;
+    }
+
+    public void hideFromPublic() {
+        this.publicLog = false;
     }
 
     void printSummary() {
-        System.out.println(this.title + " - " + this.minutes + "분");
+        // 3항 연산식: 조건식의 결과에 따라 변수에 대입할 값을 다르게 할 수 있는 문법
+        // 논리형 조건식 ? 좌항 : 우항;
+        // 논리형 조건식이 true라면 좌항의 값이, false라면 우항의 값이 도출된다.
+        String visibiliyty = publicLog ? "공개" : "비공개";
+        System.out.println(this.title + " - " + this.minutes + "분" + " - " + visibiliyty);
     }
 
     boolean needsReview() {
         return this.minutes < 60;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public boolean isPublicLog() {
+        return publicLog;
     }
 }
--- src/oop/access_modifier/SprintLogApp.java
+++ src/oop/access_modifier/SprintLogApp.java
@@ -11,7 +11,15 @@ public class SprintLogApp {
 //        javaLog.minutes = -3785924
 //        javaLog.publicLog = false;
 
+        javaLog.extendStudy(200);
+        javaLog.changeTitle("Java 객체지향과 캡슐화");
+        javaLog.hideFromPublic();
 
+        gitLog.extendStudy(45);
+        gitLog.openToPublic();
+
+        javaLog.printSummary();
+        gitLog.printSummary();
     }
 
 }
