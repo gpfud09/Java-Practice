d33e22f5a21d2e523d2222ac318c6119d02aeff2
줴령 <gpfud09@gmail.com>
2026-05-13 13:39:22 +0900

    Java Stream API - Stream API의 map 실습

--- src/oop/stream/Filtering.java
+++ src/oop/stream/Filtering.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import static oop.stream.Menu.*;
+
 public class Filtering {
 
     public static void main(String[] args) {
@@ -22,7 +24,7 @@ public class Filtering {
             System.out.println(dish);
         }
          **/
-        List<Dish> dishList = Menu.MENU_LIST.stream()
+        List<Dish> dishList = MENU_LIST.stream()
 //                .filter(dish -> dish.isVegetarian())
                 .filter(Dish::isVegetarian)
                 .toList();
@@ -32,7 +34,7 @@ public class Filtering {
         System.out.println("=====================================================");
         // 메뉴 목록 중에 육류이면서 600칼로리 미만인 요리를 필터링해서 출력
         // 메서드 체이닝 (method chaining): 리턴된 결과물을 변수에 할당 없이 바로 호출하는 방법
-        Menu.MENU_LIST.stream()
+        MENU_LIST.stream()
                 .filter(dish -> dish.getType() == Dish.Type.MEAT
                 && dish.getCalories() < 600)
                 .toList()
@@ -40,14 +42,14 @@ public class Filtering {
 
         System.out.println("=====================================================");
         // 메뉴 목록에서 요리 이름이 4글자인 것만 필터링해서 출력해줘!
-        Menu.MENU_LIST.stream()
+        MENU_LIST.stream()
                 .filter(dish -> dish.getName().length() == 4)
                 .toList()
                 .forEach(System.out::println);
 
         System.out.println("=====================================================");
         // 칼로리가 300칼로리보다 큰 요리 중에 앞에 3개만 가져와
-        Menu.MENU_LIST.stream()
+        MENU_LIST.stream()
                 .filter(dish -> dish.getCalories() > 300)
                 .limit(3) // 앞에서부터 3개만 추출
                 .toList()
@@ -55,7 +57,7 @@ public class Filtering {
 
         System.out.println("=====================================================");
         // 칼로리가 300칼로리보다 큰 요리 중에 처음 2개 제끼고 가져와
-        Menu.MENU_LIST.stream()
+        MENU_LIST.stream()
                 .filter(dish -> dish.getCalories() > 300)
                 .skip(2) // 맨 앞 2개 제외
                 .toList()
